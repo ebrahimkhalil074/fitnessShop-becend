e36a761eed21453665af05e72f43de82976ada39
ebrahimkhalil074 <mdebrahimkhalil074@gmail.com>
2024-11-05 15:49:44 +0600

    try;l

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 // app.use(cors({ origin:['https://fitnes-shop.vercel.app','https://fitness-shop-becend.vercel.app'], credentials: true }));
 app.use(cors({
-  origin: '*',credentials:true
+  origin: 'https://fitnes-shop-sium.vercel.app/',credentials:true
 }));
 
 // application routes
